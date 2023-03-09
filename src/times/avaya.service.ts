f91bbc55aeb8358d27fe6bc078d0998ef0293941
fer <fernandogabrielsalguero@gmail.com>
2023-03-08 21:04:05 -0300

    main

--- src/times/avaya.service.ts
+++ src/times/avaya.service.ts
@@ -43,6 +43,8 @@ export class AvayaService {
     newAvaya.skill14 = avayaFront[i].skill14;
     newAvaya.skill15 = avayaFront[i].skill15;
     
+    // TODO: VER COMO HACER PARA QUE PONGA VALORES POR DEFECTO EN LOS CAMPOS QUE SON NULLOS
+
     const newAvayaFinal = this.avayaRepo.create(avayaFront)
     console.log('ESTO ES LO QUE RECIBE EL PUTO SERVICIO DE MIERDA', newAvayaFinal);
     return this.avayaRepo.save(newAvayaFinal)
